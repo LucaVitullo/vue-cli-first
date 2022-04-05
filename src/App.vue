fa3eecb6765ef151ac924a5728ce49333ae2914a
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-04-05 18:09:40 +0200

    bonus completato

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Title msg="Welcome to Your Vue.js App"/>
+    <!--img alt="Vue logo" src="./assets/logo.png"--> 
+    <Title msg="La mia prima app con Vue CLI!"/>
+    <JumboTron/>
   </div>
 </template>
 
 <script>
 import Title from './components/Title.vue'
+import JumboTron from './components/JumboTron.vue'
 
 export default {
   name: 'App',
   components: {
-    Title
+    Title,
+    JumboTron,
   }
 }
 </script>
---
+++ src/components/JumboTron.vue
@@ -0,0 +1,10 @@
+<template>
+<img alt="Vue logo" src="../assets/logo.png">
+
+</template>
+
+<style>
+img{
+    width: 300px;
+}
+</style>
